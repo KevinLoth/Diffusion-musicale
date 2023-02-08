8f20aae321f1b1644be9d9530f1c54eb1d1586cc
Your Name <you@example.com>
2023-02-08 15:59:37 +0100

    routes

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php 
 
-require "../Diffusion-musicale/template/_header.php";
+require "./template/_header.php";
 ?>
-
 <?php 
-require "../Diffusion-musicale/template/_footer.php";
-?>
+# Inclure les routes :
+require "./routes.php";
+
+# Récupérer le chemin de la route :
+$url = filter_var($_SERVER["REQUEST_URI"], FILTER_SANITIZE_URL);
+
+$url = explode("?", $url)[0];
+
+$url = trim($url, "/");
+
+if(array_key_exists($url, ROUTES))
+    require "./pages/" . ROUTES[$url];
+else
+    require "./pages/404.php";
