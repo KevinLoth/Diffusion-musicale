c9b7e60c8e035767ccacc3f7140d06d6d824e480
KevinLoth <you@example.com>
2023-02-16 16:41:26 +0100

    slider

---
+++ pages/_sliderhome.php
@@ -0,0 +1,101 @@
+<?php 
+    require __DIR__."/../template/_header.php";
+?>
+<main>
+    <section>
+        <h2> Bienvenue ! </h2>
+        <div class="pills-container">
+            <div class="pills">
+                <img src="../ressources/images/facebook_cover_photo_1.png" alt="imageAlbum/Sons">
+                <div class="pills-content">
+                    <p>Super album/son </p>
+                    <p>Super artiste</p>
+                </div>
+            </div>
+            <div class="pills">
+                <img src="../ressources/images/facebook_cover_photo_1.png" alt="imageAlbum/Sons">
+                <div class="pills-content">
+                    <p>Super album/son </p>
+                    <p>Super artiste</p>
+                </div>
+            </div>
+            <div class="pills">
+                <img src="../ressources/images/facebook_cover_photo_1.png" alt="imageAlbum/Sons">
+                <div class="pills-content">
+                    <p>Super album/son </p>
+                    <p>Super artiste</p>
+                </div>
+            </div>
+            <div class="pills">
+                <img src="../ressources/images/facebook_cover_photo_1.png" alt="imageAlbum/Sons">
+                <div class="pills-content">
+                    <p>Super album/son </p>
+                    <p>Super artiste</p>
+                </div>
+            </div>
+        </div>
+        <h2> Joué Récemment</h2>
+        <div class="card-container">
+            <div class="card">
+                <img src="../ressources/images/facebook_cover_photo_1.png" alt="imageAlbum/Sons">
+                <div class="card-content">
+                    <p>Super album/son!</p>
+                    <p>Super artiste</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../ressources/images/facebook_cover_photo_1.png" alt="imageAlbum/Sons">
+                <div class="card-content">
+                    <p>Super album/son!</p>
+                    <p>Super artiste</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../ressources/images/facebook_cover_photo_1.png" alt="imageAlbum/Sons">
+                <div class="card-content">
+                    <p>Super album/son!</p>
+                    <p>Super artiste</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../ressources/images/facebook_cover_photo_1.png" alt="imageAlbum/Sons">
+                <div class="card-content">
+                    <p>Super album/son!</p>
+                    <p>Super artiste</p>
+                </div>
+            </div>
+        </div>
+        <h2> Ma playlist</h2>
+        <div class="card-container">
+            <div class="card">
+                <img src="../ressources/images/facebook_cover_photo_1.png" alt="imageAlbum/Sons">
+                <div class="card-content">
+                    <p>Super album/son!</p>
+                    <p>Super artiste</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../ressources/images/facebook_cover_photo_1.png" alt="imageAlbum/Sons">
+                <div class="card-content">
+                    <p>Super album/son!</p>
+                    <p>Super artiste</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../ressources/images/facebook_cover_photo_1.png" alt="imageAlbum/Sons">
+                <div class="card-content">
+                    <p>Super album/son!</p>
+                    <p>Super artiste</p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="../ressources/images/facebook_cover_photo_1.png" alt="imageAlbum/Sons">
+                <div class="card-content">
+                    <p>Super album/son!</p>
+                    <p>Super artiste</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
