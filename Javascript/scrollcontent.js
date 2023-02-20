9553b96b59d4f161eccef50806508f05206e95c7
KevinLoth <you@example.com>
2023-02-20 15:57:31 +0100

    scrollfinish

--- Javascript/scrollcontent.js
+++ Javascript/scrollcontent.js
@@ -1,30 +1,13 @@
 "use strict";
 
 let musique = document.querySelectorAll(".musique-scroll");
-let isDragging = false;
-let startPosition = 0;
-let currentTranslate = 0;
+
 
 musique.forEach((e)=>{
-    musique.addEventListener("mousedown", (e) => {
-    isDragging = true;
-    startPosition = e.clientX - musique.offsetLeft;
-    currentTranslate = parseInt(window.getComputedStyle(musique).getPropertyValue("transform").split(",")[4]);
-    musique.style.cursor = "grabbing";
-});
 
-musique.addEventListener("mousemove", (e) => {
-if (isDragging) {
-    const newPosition = e.clientX - musiques.offsetLeft;
-    const diffPosition = newPosition - startPosition;
-    musique.style.transform = `translateX(${currentTranslate + diffPosition}px)`;
-}
-});
-
-musique.addEventListener("mouseup", () => {
-    isDragging = false;
-    musique.style.cursor = "grab";
-});
+    e.addEventListener("wheel", (event) =>{
+        event.preventDefault();
+        e.scrollLeft += event.deltaY;
+    })
 })
 
-// !TODO Régler problème JS
