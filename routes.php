1dea5a8b5ab5d4ccc44f18a0eb4932bddb5e5575
KevinLoth <you@example.com>
2023-02-27 14:24:56 +0100

    suppression sliderhome.php, copie de home.php

--- routes.php
+++ routes.php
@@ -4,7 +4,6 @@ const ROUTES = [
     "" => "home.php",
     "register" => "register.php",
     "add_music" => "addmusic.php",
-    "sliderhome" => "_sliderhome.php",
     "login" => "login.php",
     "logout" => "logout.php",
     "account" => "account.php",
