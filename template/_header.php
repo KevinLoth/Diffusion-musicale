79ed3fdc1706fe1da634502d6fdd03f4b7b07149
Your Name <you@example.com>
2023-03-01 15:06:02 +0100

    routes-css

--- template/_header.php
+++ template/_header.php
@@ -6,11 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="/ressources/images/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="./stylesheets/CSS/main.css">
-    <link rel="stylesheet" href="./stylesheets/CSS/register.css">
+    <!-- <link rel="stylesheet" href="./stylesheets/CSS/register.css"> -->
     <link rel="stylesheet" href="./stylesheets/CSS/nav.css">
-    <link rel="stylesheet" href="./stylesheets/CSS/sliderhome.css">
-    <link rel="stylesheet" href="./stylesheets/CSS/addMusic.css">
-    <link rel="stylesheet" href="./stylesheets/CSS/account.css">
+    <!-- <link rel="stylesheet" href="./stylesheets/CSS/sliderhome.css"> -->
+    <!-- <link rel="stylesheet" href="./stylesheets/CSS/addMusic.css"> -->
+    <!-- <link rel="stylesheet" href="./stylesheets/CSS/account.css"> -->
+    <?php 
+        if($url = $_SERVER['REQUEST_URI'] == "/")
+        {
+            echo '<link rel="stylesheet" href="./stylesheets/CSS/sliderhome.css">';
+        }
+        else{
+            echo '<link rel="stylesheet" href="./stylesheets/CSS'.$url.'.css">';
+            echo $url;
+
+        }
+    ?>
     <link rel="stylesheet" href="./ressources/node_modules/@fortawesome/fontawesome-free/css/all.css">
     <script src="./ressources/node_modules/@fortawesome/fontawesome-free/js/all.js" defer></script>
     <script src="../Javascript/nav.js" defer></script>
@@ -18,5 +29,3 @@
 </head>
 <body>
     <?php require __DIR__.'/_nav.php'; ?>
-
-
